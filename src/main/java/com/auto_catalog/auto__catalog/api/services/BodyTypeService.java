945aa262707bfde7b25c5e0d1e4fde0bfd61c652
AleinikauArtsiom <artem27082003@mail.ru>
2024-04-08 22:34:35 +0300

    art_branch + secondBranch

--- src/main/java/com/auto_catalog/auto__catalog/api/services/BodyTypeService.java
+++ src/main/java/com/auto_catalog/auto__catalog/api/services/BodyTypeService.java
@@ -1,8 +1,8 @@
 package com.auto_catalog.auto__catalog.api.services;
-
 import com.auto_catalog.auto__catalog.api.dto.BodyTypeDto;
 import com.auto_catalog.auto__catalog.store.entity.BodyType;
 import com.auto_catalog.auto__catalog.store.repository.BodyTypeRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +18,7 @@ public class BodyTypeService {
         this.bodyTypeRepository = bodyTypeRepository;
     }
 
-    public List<BodyType> getAllModel() {
+    public List<BodyType> getAllBodyType() {
         return bodyTypeRepository.findAll();
 
     }
@@ -35,19 +35,28 @@ public class BodyTypeService {
     }
 
     public Optional<BodyType> getBodyTypeById(Long id) {
-         return bodyTypeRepository.findById(id);
+        return bodyTypeRepository.findById(id);
 
     }
 
-    public boolean createBodyType(BodyTypeDto bodyTypeDto) {
+    public boolean createBodyType(@Valid BodyTypeDto bodyTypeDto) {
         BodyType bodyType = new BodyType();
         bodyType.setName(bodyTypeDto.getName());
-        BodyType createdUser = bodyTypeRepository.save(bodyType);
-        return getBodyTypeById(createdUser.getBodyTypeId()).isPresent();
+        BodyType createdBodyType = bodyTypeRepository.save(bodyType);
+        return getBodyTypeById(createdBodyType.getBodyTypeId()).isPresent();
     }
 
-//    public boolean updateBodyType() {
-//
-//    }
+    public boolean updateBodyType(BodyType bodyType) {
+        Optional<BodyType> bodyTypeFromDBOptional = (bodyTypeRepository.findById(bodyType.getBodyTypeId()));
+        if (bodyTypeFromDBOptional.isPresent()) {
+            BodyType bodyTyperFromDB = bodyTypeFromDBOptional.get();
+            if (bodyType.getName() != null) {
+                bodyTyperFromDB.setName(bodyType.getName());
+            }
+            BodyType updatedBodyType = bodyTypeRepository.save(bodyTyperFromDB);
+            return getBodyTypeById(updatedBodyType.getBodyTypeId()).isPresent();
+        }
+        return false;
+    }
 
 }
